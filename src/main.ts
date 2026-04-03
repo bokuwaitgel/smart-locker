d29f1e0b0c91e0b5fa29ca2f6acdb5edb1d377f1
Itgel Oyunbold <itgel.o@techpack.mn>
2026-04-03 12:41:08 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import express from 'express';
+import * as express from 'express';
 
 //cors
 async function bootstrap() {
